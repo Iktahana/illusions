53a7706f220914c61001cb4d60bc551e488be9b8
Iktahana <171251543+Iktahana@users.noreply.github.com>
2026-07-19 02:27:20 +0900

    fix(auth): allow native addon CommonJS loader

--- native/as-web-authentication/index.js
+++ native/as-web-authentication/index.js
@@ -1,3 +1,5 @@
 "use strict";
 
+// Native Node addons are loaded through CommonJS rather than ESM imports.
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 module.exports = require("./build/Release/as_web_authentication.node");
